88550cd0712151f56da0582da63ffb5d2f41bd1a
erdenayozkanli <34692605+erdenayozkanli@users.noreply.github.com>
2018-12-18 22:45:31 +0300

    vuforia
    vuforia

---
+++ New Unity Project (2)/Assets/sc.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class sc : MonoBehaviour,IVirtualButtonEventHandler {
+
+    public GameObject vbBtnObj;
+        public GameObject vbBtnObj1;
+    // Use this for initialization
+    void Start()
+    {
+  
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+        //   cubeAni.GetComponent<Animator>();
+
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //  cubeAni.Play("none");
+        Debug.Log("dokundun");
+        vbBtnObj1.active = true;
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        Debug.Log("dokunmadun");
+        vbBtnObj1.active = false;
+
+    }
+}
---
+++ New Unity Project (2)/Assets/vb_anim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Vb_anim : MonoBehaviour, IVirtualButtonEventHandler {
+    public GameObject vbBtnObj;
+	// Use this for initialization
+	void Start () {
+        vbBtnObj = GameObject.Find("Laciebtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+     //   cubeAni.GetComponent<Animator>();
+	}
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {
+      //  cubeAni.Play("none");
+      vbBtnObj= GameObject.FindWithTag("kaybol");
+        vbBtnObj.SetActive(false);
+  
+    }
+
+   
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+       // cubeAni.Play("none");
+    }
+}
